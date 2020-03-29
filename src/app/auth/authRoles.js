be25e93861a3cbc2780f5c3504c5524376aa77f2
RobertLBebber <robert.l.bebber@gmail.com>
2020-03-28 23:49:37 -0400

    DB Schemas, Nav auth

--- src/app/auth/authRoles.js
+++ src/app/auth/authRoles.js
@@ -2,10 +2,17 @@
  * Authorization Roles
  */
 const authRoles = {
-    admin    : ['admin'],
-    staff    : ['admin', 'staff'],
-    user     : ['admin', 'staff', 'user'],
-    onlyGuest: ['guest']
+  admin: ["admin"],
+  staff: ["admin", "staff"],
+  user: ["admin", "staff", "electr", "electee"],
+  electr: ["admin", "staff", "electr"],
+  electee: ["admin", "staff", "electee"],
+  /**
+   * @todo Additional Roles
+   * @example [electr_admin,electr_manager, electr_staff]
+   * @example [electee_group_admin,electee_group_manager, electee_group_staff]
+   */
+  onlyGuest: ["admin", "staff", "guest"]
 };
 
 export default authRoles;
---
+++ src/app/fuse-configs/pageLocations.js
@@ -0,0 +1,26 @@
+/**
+ * Electr Pages
+ */
+export const PAGE_ELECTR_PRICING = "/pages/electr/sales-and-marketing";
+/**
+ * Authentication
+ */
+export const PAGE_LANDING_AUTH_LOCK = "/pages/landing/auth/lock";
+export const PAGE_LANDING_AUTH_LOGIN = "/pages/landing/auth/login";
+export const PAGE_LANDING_AUTH_LOGIN_2 = "/pages/landing/auth/login";
+export const PAGE_LANDING_AUTH_REGISTER = "/pages/landing/auth/register";
+export const PAGE_LANDING_AUTH_REGISTER_2 = "/pages/landing/auth/register";
+export const PAGE_LANDING_AUTH_MAILING = "/pages/landing/auth/mailing";
+export const PAGE_LANDING_AUTH_FORGOT_PASSWORD =
+  "/pages/landing/auth/forgot-password";
+export const PAGE_LANDING_AUTH_FORGOT_PASSWORD_2 =
+  "/pages/landing/auth/forgot-password-2";
+export const PAGE_LANDING_AUTH_RESET_PASSWORD =
+  "/pages/landing/auth/reset_password";
+export const PAGE_LANDING_AUTH_RESET_PASSWORD_2 =
+  "/pages/landing/auth/reset-password-2";
+
+/**
+ * Donation
+ */
+export const PAGE_LANDING_DONATE = "/pages/landing/donate";
